046b06e668e79ba054f1cbf8cebe300cb1a153ca
Motax97 <motinha970@gmail.com>
2025-04-08 21:12:20 +0100

    Create 1181.c

---
+++ 1181.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+char o;
+double m[12][12];
+int l;
+scanf("%d\n",&l);
+scanf("%c\n",&o);
+for(int i=0;i<12;i++){
+    for(int j=0;j<12;j++){
+     scanf("%lf",&m[i][j]);
+    }
+}
+double soma=0.0;
+for(int j=0;j<12;j++) soma+=m[l][j];
+if(o=='S') printf("%.1lf\n",soma);
+else printf("%.1lf\n",(soma/12.0));
+    return 0;
+}
