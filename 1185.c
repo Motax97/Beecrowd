348a348eb2e3a9dfc18b004f0f891520265acefd
Motax97 <motinha970@gmail.com>
2025-04-08 21:13:43 +0100

    Create 1185.c

---
+++ 1185.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+char o;
+double n[12][12];
+scanf("%c\n",&o);
+for(int i=0;i<12;i++){
+    for(int j=0;j<12;j++){
+     scanf("%lf",&n[i][j]);
+    }
+}
+double soma=0.0;
+for(int i=0;i<11;i++){
+    for(int j=0;j<(11-i);j++) soma+=n[i][j];
+}
+if(o=='S') printf("%.1lf\n",soma);
+else printf("%.1lf\n",(soma/66.0));
+    return 0;
+}
