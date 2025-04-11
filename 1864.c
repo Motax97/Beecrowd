4edd33238d664f31dfbf33225cf2cb22d9463e56
Motax97 <motinha970@gmail.com>
2025-04-11 18:55:34 +0100

    Create 1864.c

---
+++ 1864.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+char mensagem[34]="LIFE IS NOT A PROBLEM TO BE SOLVED";
+int n;
+scanf("%d\n",&n);
+for(int i=0;i<n;i++) printf("%c",mensagem[i]);
+printf("\n");
+    return 0;
+}
